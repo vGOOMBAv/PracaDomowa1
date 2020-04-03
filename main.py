9200a16b0a0f9fe036571f5c70a4b6235b2c0fd8
Kiryl <kfedorishkin@mail.ru>
2020-04-03 15:39:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
  
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World during the coronavirus pandemic"}
  
 @app.get("/{method}")
 async def read_item(method: str):
