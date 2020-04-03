7aa86677279b7074aa0fff756132f48fef492e0a
Kiryl <kfedorishkin@mail.ru>
2020-04-03 20:45:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ class Item(BaseModel):
 	surename:str
 
 app = FastAPI()
-
-
+a=0
 
 @app.get("/")
 def root():
@@ -27,8 +26,9 @@ def root():
 async def create_item(item: Item):
     #b_name=item.name.upper()
     #b_surname=item.surename.upper()
-    #return {"id":a,"patient":{"name":item.name,"surename":item.surename}}
-    return {"patient":{"name":item.name,"surename":item.surename}}
+    #return {"id":a,"patient":{"name":item.name,"surename":item.surename}
+    a=a+1
+    return {"id:":a,"patient":{"name":item.name,"surename":item.surename}}
 
 @app.put("/method")
 def root():
