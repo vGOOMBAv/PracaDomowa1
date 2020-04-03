c88a3d69b66b0c8db6e12303d0e5189a88e5461f
Kiryl <kfedorishkin@mail.ru>
2020-04-03 20:58:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
  
 from fastapi import FastAPI
 from pydantic import BaseModel
+from flask import Flask
 
 class Item(BaseModel):
 	name:str
@@ -26,9 +27,13 @@ async def create_item(item: Item):
     #b_name=item.name.upper()
     #b_surname=item.surename.upper()
     #return {"id":a,"patient":{"name":item.name,"surename":item.surename}
-    return {"patient":{"name":item.name,"surename":item.surename}}
+    a=a+1
+    return {"id":a,"patient":{"name":item.name,"surename":item.surename}}
 
 @app.put("/method")
 def root():
     return {"method": "PUT"}
- 
+
+@app.context_processor
+def context_processpr():
+    return dict(a=0)
