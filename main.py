29bd35bb2ad331a2130d6c84f394eec09a9a3548
Kiryl <kfedorishkin@mail.ru>
2020-04-03 20:24:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # main.py
  
 from fastapi import FastAPI
-from flask import request
+from pydantic import BaseModel
+
+class Item(BaseModel):
+	name:str
+	surename:str
 
 app = FastAPI()
+
+
 
 @app.get("/")
 def root():
@@ -17,9 +23,12 @@ def root():
 def root():
     return {"method": "DELETE"}
 
-@app.post("/method")
-def root():
-    return {"method": "POST"}
+@app.post("/patient")
+async def create_item(item: Item):
+    #b_name=item.name.upper()
+    #b_surname=item.surename.upper()
+    #return {"id":a,"patient":{"name":item.name,"surename":item.surename}}
+    return {"patient":{"name":item.name,"surename":item.surename}}
 
 @app.put("/method")
 def root():
