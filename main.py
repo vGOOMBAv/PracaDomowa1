71f85bacdc2ffd5e965e4c9577e9bbf21bdd52a8
Kiryl <kfedorishkin@mail.ru>
2020-04-04 11:38:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
  
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from flask import Flask
 
@@ -8,16 +8,35 @@ class Item(BaseModel):
 	name:str
 	surename:str
 
+class Patient():
+    name=""
+    surename=""
+    id=0
+    def __init__(self, name_e:str, surename_e:str, id_e:int):
+        self.name=name_e
+        self.surename=surename_e
+        self.id=id_e
+
 app = FastAPI()
 app.counter=0
+app.patient_list=[]
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
  
-@app.get("/method")
-def root():
-    return {"method": "GET"}
+@app.get("/patient/{id}")
+def return_patient_data(id):
+    is_patient=False
+    patient_pos=0
+    for i in range(app.patient_list.len()):
+        if(app.patient_list[i].id==id):
+            is_patient=True
+            patient_pos=i
+    if(is_patient==True):
+        return {"name": app.patient_list[patient_pos].name, "surename": app.patient_list[patient_pos].surename}
+    else:
+        raise HTTPException(status_code=404, detail="Patient not found")
  
 @app.delete("/method")
 def root():
@@ -25,11 +44,9 @@ def root():
 
 @app.post("/patient")
 async def create_item(item: Item):
-    #b_name=item.name.upper()
-    #b_surname=item.surename.upper()
-    #return {"id":a,"patient":{"name":item.name,"surename":item.surename}
     app.counter+=1
-    return {"id":app.counter,"patient":{"name":item.name,"surename":item.surename}}
+    patient = Patient(item.name,item.surename,app.counter)
+    app.patient_list.append(patient)
 
 @app.put("/method")
 def root():
