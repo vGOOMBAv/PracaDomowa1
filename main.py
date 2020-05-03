a874fe3c7001fdafefffe1f48fd176e43c627e0f
Kiryl <kfedorishkin@mail.ru>
2020-05-03 19:26:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, Response, Cookie, Depends, status, Request
 from pydantic import BaseModel
 import sqlite3
+import requests
 
 app = FastAPI()
 
@@ -10,22 +11,55 @@ class Item_tracks(BaseModel):
 
 
 
-@app.get("/tracks")
-def trucks(item:Item_tracks):
-    with sqlite3.connect('chinook.db') as connection:
-        first_element=item.per_page*item.page
-        last_element=first_element+item.per_page
-        cursor = connection.cursor()
-        data = cursor.execute(
-            f"SELECT trackid,name,albumid,mediatypeid, genreid, composer, milliseconds, bytes, unitprice FROM tracks WHERE trackid BETWEEN {first_element} AND {last_element}").fetchall()
-        return data
-    
-@app.get("/tracks/test")
-async def root():
-    with sqlite3.connect('chinook.db') as connection:
-        cursor = connection.cursor()
-        tracks = cursor.execute("SELECT name FROM tracks").fetchall()
-        return {
-            "tracks": tracks,
-            "tracks_counter": len(tracks)
-        }
+class CustomerStat(BaseModel):
+	CustomerId: int = None
+	Email: str = None
+	Phone: str = None
+	Sum: float = None
+
+class GenresStat(BaseModel):
+	Name: str = None
+	Sum: int = None
+
+class Category404(BaseModel):
+	detail: Error = Error(error="No such Category")
+
+
+
+@app.get("/sales")
+async def db_task_5(category: str=None):
+	if category == "customers":
+		cursor = app.db_connection.cursor()
+		data = cursor.execute("""
+			SELECT CustomerId, Email, Phone, SUM(Total) as Sum FROM(
+			SELECT * FROM invoices  
+			JOIN customers ON customers.CustomerId = invoices.CustomerId
+			)GROUP BY CustomerId ORDER BY Sum DESC, CustomerId ASC
+			""").fetchall()
+		content = []
+		for i in data:
+			content.append(CustomerStat(
+				CustomerId = i[0],
+				Email = i[1],
+				Phone = i[2],
+				Sum = round(i[3],2)
+				))
+		return content
+	elif category == "genres":
+		cursor = app.db_connection.cursor()
+		data = cursor.execute("""
+			SELECT Name, COUNT(GenreId) AS SUM FROM (
+			SELECT * FROM genres 
+			JOIN tracks ON tracks.GenreId = genres.GenreId
+			JOIN invoice_items ON invoice_items.TrackId = tracks.TrackId
+			)GROUP BY GenreId ORDER BY Sum DESC, Name ASC
+			""").fetchall()
+		content = []
+		for i in data:
+			content.append(GenresStat(
+				Name = i[0],
+				Sum = i[1],
+				))
+		return content
+	else:
+		return JSONResponse(status_code=404,content=jsonable_encoder(Category404()))
