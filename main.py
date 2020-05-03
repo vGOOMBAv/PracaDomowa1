76f2379d26e7e79384b3a2390e8501cdc31af5b1
Kiryl <kfedorishkin@mail.ru>
2020-05-03 19:30:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,137 @@
-from fastapi import FastAPI, HTTPException, Response, Cookie, Depends, status, Request
+from fastapi import FastAPI, Request, Response, status, Cookie, HTTPException, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-import sqlite3
-import requests
+from hashlib import sha256
+import secrets
+from routers import lecture4
+
+# for debug
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+# end
 
 app = FastAPI()
+security = HTTPBasic()
+templates = Jinja2Templates(directory="templates")
+app.secret_key = "wUYwdjICbQP70WgUpRajUwxnGChAKmRtfQgYASazava4p5In7pZpFPggdB4JDjlv"
+app.patients={}
+app.next_patient_id=0
+app.users={"trudnY":"PaC13Nt"}
+app.sessions={}
 
-class Item_tracks(BaseModel):
-    page:int
-    per_page:int
+app.include_router(lecture4.router)
+
+MESSAGE_UNAUTHORIZED = "Log in to access this page."
+
+# for debug
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    print(jsonable_encoder({"detail": exc.errors(), "body": exc.body}))
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+# end
+
+@app.get("/")
+def root():
+    return {"message": "Hello World during the coronavirus pandemic!"}
+
+def check_cookie(session_token: str = Cookie(None)):
+    if session_token not in app.sessions:
+        session_token = None
+    return session_token
+
+@app.get("/welcome")
+def welcome(request: Request, response: Response, session_token: str = Depends(check_cookie)):
+    if session_token is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return MESSAGE_UNAUTHORIZED
+    username = app.sessions[session_token]
+    return templates.TemplateResponse("welcome.html", {"request": request, "user": username})
+
+def login_check_cred(credentials: HTTPBasicCredentials = Depends(security)):
+    correct = False
+    for username, password in app.users.items():
+        correct_username = secrets.compare_digest(credentials.username, username)
+        correct_password = secrets.compare_digest(credentials.password, password)
+        if (correct_username and correct_password):
+            correct = True
+    if not correct:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect login or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
+    app.sessions[session_token]=credentials.username
+    return session_token
 
 
+#@app.get("/login") # for easier testing in the browser
+@app.post("/login")
+def login(response: Response, session_token: str = Depends(login_check_cred)):
+    response.status_code = status.HTTP_302_FOUND
+    response.headers["Location"] = "/welcome"
+    response.set_cookie(key="session_token", value=session_token)
 
-class CustomerStat(BaseModel):
-	CustomerId: int = None
-	Email: str = None
-	Phone: str = None
-	Sum: float = None
+#@app.get("/logout") # for easier testing in the browser
+@app.post("/logout")
+def logout(response: Response, session_token: str = Depends(check_cookie)):
+    if session_token is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return MESSAGE_UNAUTHORIZED
+    response.status_code = status.HTTP_302_FOUND
+    response.headers["Location"] = "/"
+    app.sessions.pop(session_token)
 
-class GenresStat(BaseModel):
-	Name: str = None
-	Sum: int = None
+@app.get("/method")
+@app.post("/method")
+@app.put("/method")
+@app.delete("/method")
+def get_method(request: Request):
+    return {"method": str(request.method)}
 
-class Category404(BaseModel):
-	detail: Error = Error(error="No such Category")
+class PatientRq(BaseModel):
+    name: str
+    surname: str
 
+# note: it is possible to use "name: str = Body(None), surname..." instead of "rq: PatientRq"
+@app.post("/patient")
+def add_patient(response: Response, rq: PatientRq, session_token: str = Depends(check_cookie)):
+    if session_token is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return MESSAGE_UNAUTHORIZED
+    pid=f"id_{app.next_patient_id}"
+    app.patients[pid]=rq.dict()
+    response.status_code = status.HTTP_302_FOUND
+    response.headers["Location"] = f"/patient/{pid}"
+    app.next_patient_id+=1
 
+@app.get("/patient")
+def get_all_patients(response: Response, session_token: str = Depends(check_cookie)):
+    if session_token is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return MESSAGE_UNAUTHORIZED
+    if len(app.patients) != 0:
+        return app.patients
+    response.status_code = status.HTTP_204_NO_CONTENT
 
-@app.get("/sales")
-async def db_task_5(category: str=None):
-	if category == "customers":
-		cursor = app.db_connection.cursor()
-		data = cursor.execute("""
-			SELECT CustomerId, Email, Phone, SUM(Total) as Sum FROM(
-			SELECT * FROM invoices  
-			JOIN customers ON customers.CustomerId = invoices.CustomerId
-			)GROUP BY CustomerId ORDER BY Sum DESC, CustomerId ASC
-			""").fetchall()
-		content = []
-		for i in data:
-			content.append(CustomerStat(
-				CustomerId = i[0],
-				Email = i[1],
-				Phone = i[2],
-				Sum = round(i[3],2)
-				))
-		return content
-	elif category == "genres":
-		cursor = app.db_connection.cursor()
-		data = cursor.execute("""
-			SELECT Name, COUNT(GenreId) AS SUM FROM (
-			SELECT * FROM genres 
-			JOIN tracks ON tracks.GenreId = genres.GenreId
-			JOIN invoice_items ON invoice_items.TrackId = tracks.TrackId
-			)GROUP BY GenreId ORDER BY Sum DESC, Name ASC
-			""").fetchall()
-		content = []
-		for i in data:
-			content.append(GenresStat(
-				Name = i[0],
-				Sum = i[1],
-				))
-		return content
-	else:
-		return JSONResponse(status_code=404,content=jsonable_encoder(Category404()))
+@app.get("/patient/{pid}")
+def get_patient(pid: str, response: Response, session_token: str = Depends(check_cookie)):
+    if session_token is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return MESSAGE_UNAUTHORIZED
+    if pid in app.patients:
+        return app.patients[pid]
+    response.status_code = status.HTTP_204_NO_CONTENT
+
+@app.delete("/patient/{pid}")
+def remove_patient(pid: str, response: Response, session_token: str = Depends(check_cookie)):
+    if session_token is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return MESSAGE_UNAUTHORIZED
+    app.patients.pop(pid, None)
+    response.status_code = status.HTTP_204_NO_CONTENT
